4ca22df3ab3c57c51afced37859af95d1ed39960
Dani <danilevinger23@gmail.com>
2023-06-06 19:57:24 -0600

    ErrorPage y Menu

---
+++ src/componentes/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <>
+
+            <h1>ERROR <br/>404</h1>
+            <p className="read-the-docs">
+                Sorry, but the page you are looking for doesn't exist
+            </p>
+            <p className="read-the-docs">
+                Verify the link
+            </p>
+        </>
+    )
+
+
+}
+
+export default Error404;
